5e3dfc481626498fed2366be834a8d596178187b
Tudor <uiv80988@vitesco.com>
2026-06-04 11:50:12 +0300

    actually managed to read pixels and the label

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,25 @@ use network_manager::*;
 
 fn main(){
 
-	//lets see if this compiles 
-	println!("something");
+	//first I think I should see if i can import one of them dataset files
+
+	let (n, rows , cols , images) = read_idx_images("data/train-images-idx3-ubyte");
+	let (m, labels) = read_idx_labels("data/train-labels-idx1-ubyte");
+	println!("Loaded {} images of size {}x{}",n, rows, cols);
+	println!("Loaded {} labels", m);
+
+
+	//now I'll try to print the first pixel of the first immage
+	//(essentially this is the data I will send to the nn)
+	//its also problably going to be white 99%
+	println!("First pixel of the immage(probably white): {}", images[0]);
+	
+	//and also the label (the number it repersents)
+	println!("The label of the first image: {}", labels[0]);
+
+
+	//now we should just declare the network and start training
+	
 
 
 }
